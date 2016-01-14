5bb8058ffb9bfdf45b16bc861396754bd21e009c
DeveloperLx <developerlx@yeah.net>
2016-01-14 17:01:51 +0800

    http support

--- LxProjectTemplateDemo/LxProjectTemplateDemo/ViewController.swift
+++ LxProjectTemplateDemo/LxProjectTemplateDemo/ViewController.swift
@@ -14,23 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
-        
-        Alamofire.request(.GET, "https://httpbin.org/get", parameters: ["foo": "bar"])
-            .responseJSON {response in
-                print(response.request)  // original URL request
-                print(response.response) // URL response
-                print(response.data)     // server data
-                print(response.result)   // result of response serialization
-                
-                if let JSON = response.result.value {
-                    print("JSON: \(JSON)")
-                }
-                
-                debugPrint(response)
-        }
+      
     }
-
-
 }
 
